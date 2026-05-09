c673b187b9f02aba00747cd6d5ea996e2c36f6ff
jundullah-7 <rayhankamal729426@gmail.com>
2026-05-09 21:28:56 +0600

    Initial Project Setup with .gitignore and app.java

---
+++ src/App.java
@@ -0,0 +1,9 @@
+    package main;
+
+    public class App {
+
+        public static void main(String[] args) {
+
+            System.out.println("LFMS Project Started Successfully");
+        }
+    }
